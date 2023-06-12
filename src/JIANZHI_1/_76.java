43b97c73145b2d1ac0d96d3b00b63d09fca4caf7
Feiyu Qiao <z5324320@ad.unsw.edu.au>
2023-06-12 11:02:13 +1000

    _612复健

--- src/JIANZHI_1/_76.java
+++ src/JIANZHI_1/_76.java
@@ -1,9 +1,15 @@
 package JIANZHI_1;
 
+import java.util.Arrays;
+
 /**
+ * 找数组中第k大的数
  * @author 乔飞宇
  * @version 1.0
  */
 public class _76 {
-
+    public int findKthLargest(int[] nums, int k) {
+        Arrays.sort(nums);
+        return nums[nums.length - 4];
+    }
 }
